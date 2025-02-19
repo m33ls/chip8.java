b35aeab049d335a3aac0225d2f0138b14086c015
m33ls <70194930+m33ls@users.noreply.github.com>
2025-02-18 19:58:43 -0800

    test fontset

--- Chip8Tester.java
+++ Chip8Tester.java
@@ -1,19 +1,42 @@
 public class Chip8Tester
 {
-	public static void assertEq(int actual, int expected)
+	public static void assertEq(String testName, int actual, int expected)
 	{
 		if (actual == expected) {
-			System.out.println("[SUCCESS]");
+			System.out.println("[SUCCESS] Test." + testName + " " + actual + " == " + expected);
 		} 
 		else {
-			System.out.println("[FAILED] " + actual + " not equal to " + expected);
+			System.out.println("[FAILED] Test." + testName + " " + actual + " != " + expected);
 		}
 	}
 
 	public static void main(String[] args)
 	{
 		Chip8 myChip8 = new Chip8();
+
+		// Test fontset
 		myChip8.loadFontset();
-		assertEq(1,2);
+		int[] fontsetTest = {
+			0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+                        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+                        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+                        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+                        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+                        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+                        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+                        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+                        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+                        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+                        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+                        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+                        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+                        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+                        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+                        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+		};
+		for (int i = 0; i < 80; i++) {
+			assertEq("myChip8.loadFontset()", myChip8.memory[i], fontsetTest[i]);
+		}
+
 	}
 }
